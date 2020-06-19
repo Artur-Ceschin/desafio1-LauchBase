4c605dc79b61cf84b5d57a4f0e3718a95b9611b1
Artur Ceschin <artur.ceschin@gmail.com>
2020-06-19 17:23:35 -0300

    Aula sobre vetores

---
+++ functions.js
@@ -0,0 +1,63 @@
+//Functions Aula
+//Estruturas de repetição
+//Escopo
+
+const alunos = [
+  {
+    nome: "Artur",
+    nota: 8.8,
+  },
+  {
+    nome: "Fernanda",
+    nota: 3,
+  },
+  {
+    nome: "Maria",
+    nota: 5,
+  },
+  {
+    nome: "Petra",
+    nota: 8,
+  },
+];
+
+const alunos2 = [
+  {
+    nome: "Pedro",
+    nota: 9.8,
+  },
+  {
+    nome: "Carla",
+    nota: 6,
+  },
+  {
+    nome: "Silvia",
+    nota: 5,
+  },
+  {
+    nome: "João",
+    nota: 5,
+  },
+];
+
+function calculaMedia(alunos) {
+  let soma = 0;
+  for (let i = 0; i < alunos.length; i++) {
+    soma = soma + alunos[i].nota;
+  }
+  const media = soma / alunos.length;
+  return media;
+}
+const media1 = calculaMedia(alunos);
+const media2 = calculaMedia(alunos2);
+
+function enviaMensagem(media, turma) {
+  if (media > 6) {
+    console.log(`A media da turma ${turma} foi ${media}.`);
+  } else {
+    console.log(`A média da turma ${turma} foi menor que 6`);
+  }
+}
+
+enviaMensagem(media1, "TurmaA");
+enviaMensagem(media2, "TurmaB");
